d7a6427a8b948c09730e46830686b1d52d285dec
saurako <kssaurabh@gmail.com>
2018-12-05 12:34:56 -0800

    First pass: false event handling

--- src/org/traccar/processing/peripheralsensorprocessors/fuelsensorprocessors/FalseEventChecker.java
+++ src/org/traccar/processing/peripheralsensorprocessors/fuelsensorprocessors/FalseEventChecker.java
@@ -1,42 +1,45 @@
 package org.traccar.processing.peripheralsensorprocessors.fuelsensorprocessors;
 
 import com.google.common.collect.Lists;
+import org.traccar.Context;
 import org.traccar.helper.Log;
+import org.traccar.model.Event;
 import org.traccar.model.Position;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class FalseEventChecker {
 
+    private static final double FALSE_ALERT_FUEL_DIFF_THRESHOLD = 0.10;
     private static int WINDOW_SIZE_MILLIS = 600_000;
 
-    private static FalseEventChecker falseEventCheckerInstance = null;
-
-    private static Map<Long, List<FuelActivity>> DEVICE_ALERT_PENDING_MAP = new ConcurrentHashMap<>();
+    private static Map<Long, List<FuelActivity>> DEVICE_PENDING_ALERTS_LIST = new ConcurrentHashMap<>();
 
     private FalseEventChecker() {}
 
-    public static void addEvent(long deviceId, FuelActivity fuelActivity) {
-        if (!DEVICE_ALERT_PENDING_MAP.containsKey(deviceId)) {
+    public static void addEventToPendingList(long deviceId, FuelActivity fuelActivity) {
+        if (!DEVICE_PENDING_ALERTS_LIST.containsKey(deviceId)) {
             List<FuelActivity> pendingList = Lists.newArrayList();
-            DEVICE_ALERT_PENDING_MAP.put(deviceId, pendingList);
+            DEVICE_PENDING_ALERTS_LIST.put(deviceId, pendingList);
         }
 
-        DEVICE_ALERT_PENDING_MAP.get(deviceId).add(fuelActivity);
+        DEVICE_PENDING_ALERTS_LIST.get(deviceId).add(fuelActivity);
     }
 
     public static int pendingListSize(long deviceId) {
-        return DEVICE_ALERT_PENDING_MAP.containsKey(deviceId)? DEVICE_ALERT_PENDING_MAP.get(deviceId).size() : 0;
+        return DEVICE_PENDING_ALERTS_LIST.containsKey(deviceId)? DEVICE_PENDING_ALERTS_LIST.get(deviceId).size() : 0;
     }
 
-    public static void removeEvents(long deviceId, int startIndex, int endIndex) {
-        if (!DEVICE_ALERT_PENDING_MAP.containsKey(deviceId)) {
+    public static void removeEventFromPendingList(long deviceId, int startIndex, int endIndex) {
+        if (!DEVICE_PENDING_ALERTS_LIST.containsKey(deviceId)) {
             Log.debug(String.format("Device %d does not have a pending events list yet", deviceId));
             return;
         }
-        List<FuelActivity> pendingList = DEVICE_ALERT_PENDING_MAP.get(deviceId);
+        List<FuelActivity> pendingList = DEVICE_PENDING_ALERTS_LIST.get(deviceId);
 
         boolean validBoundaries = validateIndices(startIndex, endIndex, pendingList);
 
@@ -60,11 +63,11 @@ public class FalseEventChecker {
     }
 
     public static void removeEvent(long deviceId, int index) {
-        if (!DEVICE_ALERT_PENDING_MAP.containsKey(deviceId)) {
+        if (!DEVICE_PENDING_ALERTS_LIST.containsKey(deviceId)) {
             Log.debug(String.format("Device %d does not have a pending events list yet", deviceId));
             return;
         }
-        List<FuelActivity> pendingList = DEVICE_ALERT_PENDING_MAP.get(deviceId);
+        List<FuelActivity> pendingList = DEVICE_PENDING_ALERTS_LIST.get(deviceId);
 
         if (index >= pendingList.size()) {
             Log.debug(String.format("Pending list for device %d is smaller than requested index %d: size = %d",
@@ -72,18 +75,18 @@ public class FalseEventChecker {
             return;
         }
 
-        DEVICE_ALERT_PENDING_MAP.get(deviceId).remove(index);
+        DEVICE_PENDING_ALERTS_LIST.get(deviceId).remove(index);
     }
 
-    public void checkFalseAlert(Position position) {
+    public static void checkAndReportFalseAlerts(Position position) {
         long deviceId = position.getDeviceId();
 
-        if (!DEVICE_ALERT_PENDING_MAP.containsKey(deviceId)) {
+        if (!DEVICE_PENDING_ALERTS_LIST.containsKey(deviceId)) {
             Log.debug(String.format("Device %d does not have a pending events list yet", deviceId));
             return;
         }
 
-        List<FuelActivity> pendingList = DEVICE_ALERT_PENDING_MAP.get(deviceId);
+        List<FuelActivity> pendingList = DEVICE_PENDING_ALERTS_LIST.get(deviceId);
 
         if (position.getDeviceTime().getTime() - pendingList.get(0).getActivityEndTime().getTime() < WINDOW_SIZE_MILLIS) {
             Log.debug(String.format("[DeviceId: %d]: Time difference between first event and current too small",
@@ -92,11 +95,97 @@ public class FalseEventChecker {
         }
 
         if (pendingList.size() == 1) {
+            // Will remove the first item in the pendingList and use that to send an alert.
             FuelEventNotifier.sendNotificationIfNecessary(deviceId, pendingList.remove(0));
             return;
         }
 
+        for (int i = 1; i < pendingList.size() - 1; i++) {
+            double checkFuelLevel = pendingList.get(i).getActivityEndPosition().getDouble(Position.KEY_CALIBRATED_FUEL_LEVEL);
+            double consumedFuel = 0.0;
 
+            for (int j = i-1; j >= 0; j--) {
+                FuelActivity startActivity = pendingList.get(j-1);
+                FuelActivity endActivity = pendingList.get(j);
 
+                double startLevel = startActivity.getActivityStartPosition().getDouble(Position.KEY_CALIBRATED_FUEL_LEVEL);
+                double endLevel = endActivity.getActivityEndPosition().getDouble(Position.KEY_CALIBRATED_FUEL_LEVEL);
+
+                double diffInFuelLevels = endLevel - startLevel;
+
+                if (diffInFuelLevels > 0) {
+                    consumedFuel += diffInFuelLevels;
+                }
+
+                double minFuelLevel = (checkFuelLevel - consumedFuel)* (1.0 - FALSE_ALERT_FUEL_DIFF_THRESHOLD);
+                double maxFuelLevel = (checkFuelLevel - consumedFuel)* (1.0 + FALSE_ALERT_FUEL_DIFF_THRESHOLD);
+
+                if (endActivity.getActivityStartPosition().getDouble(Position.KEY_CALIBRATED_FUEL_LEVEL) > minFuelLevel
+                    && endActivity.getActivityStartPosition().getDouble(Position.KEY_CALIBRATED_FUEL_LEVEL) < maxFuelLevel) {
+
+                    // Possible false alerts
+                    for (int a = i; a <= j; a++) {
+                        FuelEventNotifier.sendNotificationIfNecessary(deviceId, pendingList.remove(a));
+                    }
+                    break;
+                }
+            }
+        }
+
+        for (int i = 1; i < pendingList.size() - 1; i++) {
+
+            if (position.getDeviceTime().getTime() - pendingList.get(0).getActivityEndPosition().getDeviceTime().getTime() <= WINDOW_SIZE_MILLIS) {
+                continue;
+            }
+
+            FuelEventNotifier.sendNotificationIfNecessary(deviceId, pendingList.remove(0));
+        }
+    }
+
+    public static void falseAlertsBeyondWindow(Position position) {
+        long deviceId = position.getDeviceId();
+        try {
+            Optional<Event> mostRecentFuelEvent = Context.getDataManager().getMostRecentFuelEvent(deviceId);
+            mostRecentFuelEvent.ifPresent(event -> handleEventBeyondWindow(event, deviceId));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void handleEventBeyondWindow(Event previousAlert, long deviceId) {
+
+        if (!DEVICE_PENDING_ALERTS_LIST.containsKey(deviceId)) {
+            Log.debug(String.format("Device %d does not have a pending events list yet", deviceId));
+            return;
+        }
+
+        List<FuelActivity> pendingList = DEVICE_PENDING_ALERTS_LIST.get(deviceId);
+        Position fuelActivityEndPosition = pendingList.get(0).getActivityEndPosition();
+        Position fuelActivityStartPosition = pendingList.get(0).getActivityStartPosition();
+
+        double activityStartFuelLevel = fuelActivityStartPosition.getDouble(Position.KEY_CALIBRATED_FUEL_LEVEL);
+        double checkFuelLevel = fuelActivityEndPosition.getDouble(Position.KEY_CALIBRATED_FUEL_LEVEL);
+
+        try {
+            Optional<Position> previousAlertStartPosition = Context.getDataManager().getPositionById(previousAlert.getLong("startPositionId"));
+            Optional<Position> previousAlertEndPosition = Context.getDataManager().getPositionById(previousAlert.getLong("endPositionId"));
+
+            if (previousAlertStartPosition.isPresent() && previousAlertEndPosition.isPresent()) {
+                double previousAlertEndFuelLevel = previousAlertEndPosition.get().getDouble(Position.KEY_CALIBRATED_FUEL_LEVEL);
+                double previousAlertStartFuelLevel = previousAlertStartPosition.get().getDouble(Position.KEY_CALIBRATED_FUEL_LEVEL);
+
+                double consumedFuel = previousAlertEndFuelLevel - activityStartFuelLevel;
+                double minFuelLevel = (checkFuelLevel - consumedFuel)* (1.0 - FALSE_ALERT_FUEL_DIFF_THRESHOLD);
+                double maxFuelLevel = (checkFuelLevel - consumedFuel)* (1.0 + FALSE_ALERT_FUEL_DIFF_THRESHOLD);
+
+                if (previousAlertStartFuelLevel > minFuelLevel && previousAlertStartFuelLevel < maxFuelLevel) {
+                    FuelEventNotifier.sendNotificationIfNecessary(deviceId, pendingList.remove(0), true);
+                } else {
+                    FuelEventNotifier.sendNotificationIfNecessary(deviceId, pendingList.remove(0));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
